8140bc878babc3e8b489563e9866aa773f78423d
HimanshuKumarSingh14 <himanshukumar14122004@gmail.com>
2026-06-09 21:53:18 +0530

    move_zeros_to_end

---
+++ Day4/move_zeros_to_end.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter the number elements in the array:"<<endl;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    int j=-1;
+    for(int i=0;i<n;i++)
+    {
+        if(arr[i]==0)
+        {
+            j=i;
+            break;
+        }
+    }
+    for(int i=j+1;i<n;i++)
+    {
+        if(arr[i]!=0)
+        {
+            swap(arr[i],arr[j]);
+            j++;
+        }
+    }
+    //printing the array elements after moving the elements to the right end
+    for(int i=0;i<n;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
