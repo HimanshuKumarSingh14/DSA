f2b5a574869231e0af64de38399a927f2a451146
HimanshuKumarSingh14 <himanshukumar14122004@gmail.com>
2026-07-03 23:03:41 +0530

    largest_smallest

---
+++ c_based_coding/largest_smallest.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    sort(arr,arr+n);
+    cout<<"Largest"<<arr[n-1]<<endl;
+    cout<<"Smallest"<<arr[0];
+    return 0;
+}
