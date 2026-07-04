babe72933ac181a51cad42894fa313bc37377e4c
HimanshuKumarSingh14 <himanshukumar14122004@gmail.com>
2026-07-04 19:26:35 +0530

    printnto1backtrack

---
+++ recursion/printnto1backtrack.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+void fun(int i,int n)
+{
+    if(i>n)
+    return;
+    fun(i+1,n);
+    cout<<i<<endl;
+}
+int main()
+{
+    int n;
+    cin>>n;
+    fun(1,n);
+    return 0;
+}
