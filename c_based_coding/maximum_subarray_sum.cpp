6f131999c60da8a977cdb8504209548d328241ef
HimanshuKumarSingh14 <himanshukumar14122004@gmail.com>
2026-07-04 18:47:54 +0530

    print_name_ntimes

--- c_based_coding/maximum_subarray_sum.cpp
+++ c_based_coding/maximum_subarray_sum.cpp
@@ -17,7 +17,7 @@ int main()
     for(int i=0;i<n;i++)
     {
         if(sum==0)
-         start=i;
+        start=i;
         sum+=arr[i];
         if(sum>maxi)
         {
---
+++ recursion/print_name_ntimes.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+void fun(int i,int n)
+{
+    if(i>n)
+    return ;
+    cout<<"Manshu"<<endl;
+    fun(i+1,n);
+}
+int main()
+{
+    int n;
+    cin>>n;
+    fun(1,n);
+}
