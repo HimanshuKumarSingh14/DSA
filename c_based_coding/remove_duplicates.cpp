05b6838ea76a8ef8a441162a4970975f634835b5
HimanshuKumarSingh14 <himanshukumar14122004@gmail.com>
2026-07-04 11:20:05 +0530

    remove_duplicates

---
+++ c_based_coding/remove_duplicates.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<set>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+
+    }
+    set<int>st;
+    for(int i=0;i<n;i++)
+    {
+        st.insert(arr[i]);
+    }
+    int index=0;
+    for(auto i:st)
+    {
+        arr[index]=i;
+        index++;
+    }
+    for(int i=0;i<index;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
